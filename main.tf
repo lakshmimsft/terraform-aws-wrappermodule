4de881bb60ff8417619996fb70f1be65d5953aa9
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-02-11 21:40:18 -0800

    update

--- main.tf
+++ main.tf
@@ -11,10 +11,30 @@ provider "aws" {
   region = "eu-west-2"  # Replace with your desired region
 }
 
+data "aws_caller_identity" "current" {}
+
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 2.0"
-  
+  version = "3.15.1"
+
   bucket = var.bucket_name
-  # Other module arguments...
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+  expected_bucket_owner    = data.aws_caller_identity.current.account_id
+  acl                      = "public-read"
+
+  cors_rule = [
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["GET"]
+      allowed_origins = ["https://my-app.domain.com"]
+      max_age_seconds = 3000
+    }
+  ]
 }
